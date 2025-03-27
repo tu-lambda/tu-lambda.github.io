fcd2dbec6ecfae78814b9ecd590482894c57efaa
Beat Hagenlocher <bah@posteo.de>
2025-03-27 16:45:32 +0100

    First step of nixification

--- flake.nix
+++ flake.nix
@@ -24,11 +24,35 @@
         # system.
 
         # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
-        packages.default = pkgs.hello;
+        apps.default = {
+          type = "app";
+          program = "${pkgs.lib.getExe self'.packages.rps}";
+        };
+        packages = {
+          rps = pkgs.writeShellScriptBin "rps" ''
+            ${pkgs.racket}/bin/raco pkg install --auto --skip-installed pollen string-interpolation && ${pkgs.racket}/bin/raco pollen start
+          '';
+          default = pkgs.stdenv.mkDerivation {
+            name = "tu-lambda.github.io";
+            src = ./.;
+
+            nativeBuildInputs = [ pkgs.cacert ];
+            buildInputs = [ pkgs.racket ];
+            buildPhase = ''
+              export HOME=$(pwd)
+              raco pkg install --auto --skip-installed pollen string-interpolation
+              raco pollen render
+            '';
+            installPhase = ''
+              mkdir $out
+              cp -r assets *.html *.css *.js $out
+            '';
+            outputHashAlgo = "sha256";
+            outputHashMode = "recursive";
+            outputHash = "sha256-IqePV4oF9/XKDnn+BWIstScY7DDD4YUBkBIv1InMO0o=";
+          };
+        };
         devshells.default = {
-          env = [
-              # { name = "MY_ENV_VAR"; value = "SOTRUE"; }
-          ];
           packages = [
             pkgs.racket
           ];
@@ -36,16 +60,12 @@
             install-langserver.text = ''raco pkg install --auto --skip-installed racket-langserver'';
             install-web.text = ''raco pkg install --auto --skip-installed pollen string-interpolation'';
           };
-          commands = [
-            { name = "rps"; command = "raco pollen start"; help = "Run the pollen web server"; }
-          ];
         };
       };
       flake = {
         # The usual flake attributes can be defined here, including system-
         # agnostic ones like nixosModule and system-enumerating ones, although
         # those are more easily expressed in perSystem.
-
       };
     };
 }
